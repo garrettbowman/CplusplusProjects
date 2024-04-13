22a40ec186620f3b17089c4d87ea01f17869059e
garrett <garrettbowman96@gmail.com>
2024-04-13 05:02:41 -0400

    first commit

---
+++ helloworld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //comment
+    std::cout << "Hello, World!"<< std::endl;
+    std::cout << "Im gary!" << '\n';
+
+    int x = 0; //declare
+    x = 5; //assign
+    std::cout << x << std::endl;
+
+    return 0;
+}
