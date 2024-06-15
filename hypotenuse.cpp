212f470f249ea4f64210653402cd04f83155d29b
garrett <garrettbowman96@gmail.com>
2024-06-15 00:58:27 -0400

    hypotenuse calc

---
+++ hypotenuse.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+namespace lol {
+    int x = 0;
+}
+
+int main()
+{
+    //comment 2 lol
+    std::cout << "Hello, World!"<< std::endl;
+    std::cout << "Im gary!" << '\n';
+
+    int x = 0; //declare
+    x = 5; //assign
+    std::cout << x << std::endl;
+
+    return 0;
+}
